138b0a0ba0a64c46c9f193c5de3c6297ed783aa6
Peter Tollnes Flem <peter.t.flem@gmail.com>
2016-10-25 21:47:10 +0200

    Working on the contact page.

--- vmcwp/wp-content/themes/customizr-child/page-contact.php
+++ vmcwp/wp-content/themes/customizr-child/page-contact.php
@@ -30,93 +30,41 @@ Template Name: Contact Page
 
                 <div id="content" class="<?php echo implode(' ', apply_filters( 'tc_article_container_class' , array( TC_utils::tc_get_layout(  TC_utils::tc_id() , 'class' ) , 'article-container' ) ) ) ?>">
 
-                  <div class="row-fluid hidden-phone">
-                    <div class="span3 reb1 contact-picture"></div>
-                    <div class="span3 reb2 contact-picture"></div>
-                    <div class="span3 reb3 contact-picture"></div>
-                    <div class="span3 reb4 contact-picture"></div>
-                  </div>
 
-                  <div class="row-fluid contact-information hidden-phone">
-                    <div class="contact-information-list">
-                      <ul>
-                        <li class="name">Rebecca Hansen</li>
-                        <li class="title">Lederutvikler</li>
-                        <li class="phonenumber">
-                          <a href="tel:+4793845559">+47 938 45 559</a>
-                        </li>
-                        <li class="email">
-                          <a href="mailto:rebecce@vmc.no">rebecce@vmc.no</a>
-                        </li>
-                        <li class="linkedin">
-                          <a href="#">LinkedIn</a>
-                        </li>
-                        <li>
-                          <label role="button" class="open-contact-form" for="toggle-modal">Kontaktskjema</label>
-                          <input type="checkbox" id="toggle-modal" />
 
-                          <label role="button" class="modal-background" for="toggle-modal"></label>
+                  <div class="row-fluid contact-information">
+                    <h1>Rebecca Hansen</h1>
+                    <h2 class="title">Lederutvikler</h2>
+                    <a href="tel:+4793845559">+47 938 45 559</a>
+                    <a href="mailto:rebecca@vmconsulting.no">rebecca@vmconsulting.no</a>
+                    <a href="#">LinkedIn</a>
 
-                          <div class="modal">
-                            <div class="modal-header">
-                              <h2>Kontaktskjema</h2>
-                            </div>
-                            <div class="modal-body">
-                              <?php if (have_posts()) : while (have_posts()) : the_post();?>
-                                <?php the_content(); ?>
-                              <?php endwhile; endif; ?>
-                            </div>
-                            <div class="modal-footer">
-                              <label class="btn" for="toggle-modal" role="button">Lukk</label>
-                            </div>
-                          </div>
-                        </li>
-                      </ul>
+                    <div class="contact-form">
+                      <label role="button" class="open-contact-form" for="toggle-modal">Kontaktskjema</label>
+                      <input type="checkbox" id="toggle-modal" />
+                      <label role="button" class="modal-background" for="toggle-modal"></label>
+                      <div class="modal">
+                        <div class="modal-header">
+                          <h2>Kontaktskjema</h2>
+                        </div>
+                        <div class="modal-body">
+                          <?php if (have_posts()) : while (have_posts()) : the_post();?>
+                            <?php the_content(); ?>
+                          <?php endwhile; endif; ?>
+                        </div>
+                        <div class="modal-footer">
+                          <label class="btn" for="toggle-modal" role="button">Lukk</label>
+                        </div>
+                      </div>
                     </div>
                   </div>
 
                   <div class="row-fluid">
                     <div class="span12">
-                      <dev class="visible-phone">
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/reb2.jpg" class="profile-picture" alt="Bilde av Rebecca Hansen" />
+                      <dev>
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/contact-page-profile-picture.jpg" class="profile-picture" alt="Bilde av Rebecca Hansen" />
                       </div>
                     </div>
-                  </div>
-
-                  <div class="row-fluid contact-information visible-phone">
-                    <ul>
-                      <li class="name">Rebecca Hansen</li>
-                      <li class="title">Lederutvikler</li>
-                      <li>
-                        <a href="tel:+4793845559">+47 938 45 559</a>
-                      </li>
-                      <li>
-                        <a href="mailto:rebecce@vmc.no">rebecce@vmc.no</a>
-                      </li>
-                      <li>
-                        <a href="#">LinkedIn</a>
-                      </li>
-                      <li class="contact-form-mobile">
-                        <label role="button" class="open-contact-form" for="toggle-modal">Kontaktskjema</label>
-                        <input type="checkbox" id="toggle-modal" />
-
-                        <label role="button" class="modal-background" for="toggle-modal"></label>
-
-                        <div class="modal">
-                          <div class="modal-header">
-                            <h2>Kontaktskjema</h2>
-                          </div>
-                          <div class="modal-body">
-                            <?php if (have_posts()) : while (have_posts()) : the_post();?>
-                              <?php the_content(); ?>
-                            <?php endwhile; endif; ?>
-                          </div>
-                          <div class="modal-footer">
-                            <label class="btn" for="toggle-modal" role="button">Lukk</label>
-                          </div>
-                        </div>
-                      </li>
-                    </ul>
                   </div>
 
                 </div><!--.article-container -->
