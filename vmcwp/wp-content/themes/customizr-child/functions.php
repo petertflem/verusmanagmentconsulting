371ae7ca2783481b554f49261e482a393c9e5e99
Peter Tollnes Flem <peter.t.flem@gmail.com>
2016-10-25 21:52:54 +0200

    Enabled automatic updates (I think).

---
+++ vmcwp/wp-content/themes/customizr-child/functions.php
@@ -0,0 +1,6 @@
+<?php
+
+// Enable automatic updates
+add_filter( 'auto_update_plugin', '__return_true' );
+add_filter( 'auto_update_theme', '__return_true' );
+define( 'WP_AUTO_UPDATE_CORE', true );
